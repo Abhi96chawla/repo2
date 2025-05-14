6d52ba8237c5827607a8b8ef47305ef3b74839c0
Abhi96chawla <chawlaa019@gmail.com>
2025-05-14 19:24:06 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git and Java! Repo2");
+        System.out.println("------ Repo2  -------------");
     }
 }
